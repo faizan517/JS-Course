2b49d67c19264d6d0205fd71e25688a57a3a2dbe
faizan517 <faizanpervaiz517@gmail.com>
2020-02-19 18:35:38 +0500

    assignment-5

--- assignment_5/js/app.js
+++ assignment_5/js/app.js
@@ -51,7 +51,6 @@ if (colorrt == "red") {
 
 //Question 6
 
-
 /*var maxAge = prompt("Enter max age")
 var age = prompt("Enter your age");
 if (maxAge >=age ) {
@@ -60,7 +59,20 @@ if (maxAge >=age ) {
   alert("not allowed");
 }*/
 
-var totalMarks = 300;
+//Question 7
+
+
+/*var fuel = +prompt("Enter remaning fuel")
+if (fuel < 0.25 ) {
+  alert("refill the fuel");
+} else {
+  alert("don,t need refiil");
+}*/
+
+
+//Question 9
+
+/*var totalMarks = 300;
 var subject1 = +prompt("Urdu marks")
 var subject2 = +prompt("English marks")
 var subject3 = +prompt("Math marks")
@@ -68,16 +80,279 @@ var subject3 = +prompt("Math marks")
 var obtainedMarks = (subject1+subject2+subject3)
 
 var percentage =(obtainedMarks/totalMarks)*100;
+
+console.log("MARKSHEET")
+console.log("Total MArks:",totalMarks)
+console.log("Obtained Marks:" ,obtainedMarks)
+
+
 if(percentage>=80){
-  console.log("A-one","Excellent")
+  console.log("Percentage:","A-one")
+  console.log("Re-Marks:","Excellent")
 }
 else if(percentage>=70){
-  document.write("A","Good")
+  console.log("Percentage:","A")
+  console.log("Re-Marks:","Good")
 }
 else if(percentage>=60){
-  document.write("B","You need to improve")
+  console.log("Percentage:","B")
+  console.log("Re-Marks:","You need to improve")
 }
 else {
-  document.write("FAIL","sorry")
+  console.log("Percentage:","FAIL")
+  console.log("Re-Marks:","Sorry")
+}*/
+
+//Question 10
+
+/*var name1=prompt("name of item 1")
+var item1=+prompt("price of item 1")
+var que1=+prompt("quantity of item 1")
+
+
+var name2=prompt("name of item 2")
+var item2=+prompt("price of item 2")
+var que2=+prompt("quantity of item 2")
+
+
+var add2 = (item2*que2)
+var add1 = (item1*que1)
+var totalAmount= (add1+add2);
+var shipping =+prompt("shipping charges")
+
+console.log("Price of",name1,item1)
+console.log("Quantity of",name1 ,que1)
+
+console.log("Price of",name2,item2)
+console.log("Quantity of",name2 ,que2)
+
+console.log("Shipping Charges:",shipping)
+console.log("total amount:",totalAmount+shipping)
+
+if(totalAmount>=2000){
+var discount1=(totalAmount*0.10)
 }
-document.write("MARKSHEET")
+else{
+console.log("Discounted price:",0,"PKR")
+}
+
+if(discount1)
+var totalCost = (totalAmount - discount1)
+console.log("Discounted price:",totalCost,"PKR",)
+}*/
+
+
+//Question 11
+
+/*var secret = +prompt("secret number (1-10)");
+
+if(secret==4){
+  console.log("so close")
+}
+else if(secret==6){
+  console.log("so close")
+}
+else if(secret==5){
+  console.log("correct answer")
+}
+else{
+  console.log("Try again")
+}*/
+
+//Question 12
+
+/*var divide = prompt("enter divide number")
+var num = 3
+if(divide%3===0){
+  console.log("Number is divided by 3")
+}
+else{
+  console.log("Number is not divided")
+}*/
+
+//Question 13
+
+
+/*var name1 =prompt("Enter first team name")
+var TotalScore1 = +prompt("Enter total score first team")
+
+var name2 =prompt("Enter first team name")
+var TotalScore2 = +prompt("Enter total score first team")
+
+if(TotalScore1>=TotalScore2){
+ console.log(name1,"have won the game")
+}
+else if(TotalScore1<=TotalScore2){
+console.log(name2,"have won the game")
+}
+else{
+  console.log("Match tie")
+}*/
+
+//Question 14
+
+/*var string =("string")
+var number = ("number")
+var boolean =("boolean")
+
+var p = prompt("Enter")
+if(typeof(p) === 'string'){
+  
+  console.log(p ,"is string")
+}
+else if (typeof(p) === 'number'){
+  console.log(p,"is number")
+}
+else if (p==="ture"||"false"){
+  console.log(p,"is boolean")
+}*/
+
+
+//Question 15
+
+
+/*var num =+prompt("enter number")
+if(num%2===0){
+  console.log("givin number is even")
+}
+else{
+  console.log("givin number is odd")
+}*/
+
+//Question 16
+
+/*var temp = +prompt("Enter temperature")
+if (temp>=40){
+  console.log("It is too hot outside")
+}
+else if (temp>=30){
+  console.log("The weather today is normal")
+}
+else if (temp>=20){
+  console.log("Today's weather is cool")
+}
+else {
+  console.log("OMG!Today's weather is so cool")
+}*/
+
+
+//Question 17
+
+
+//var firstNum = +prompt('First number')
+//var operation = prompt("+,-,*/,%")
+//var secondNum = +prompt('Second number')
+
+/* if (operation==='-'){
+  console.log(firstNum,"-",secondNum,"=",firstNum-secondNum)
+}
+else if (operation==='*'){
+  console.log(firstNum,"*",secondNum,"=",firstNum*secondNum)
+}
+else if (operation==='%'){
+  console.log(firstNum,"%",secondNum,"=",firstNum%secondNum)
+}
+else if (operation==='/'){
+  console.log(firstNum,"/",secondNum,"=",firstNum/secondNum)
+}
+else if (operation==='+'){
+  console.log(firstNum,"+",secondNum,"=",firstNum+secondNum)
+  }  
+else {
+  console.log("try again")
+}*/
+
+//Question 18
+
+
+/*var input = prompt("Enter day")
+
+if(input=="monday"){
+  console.log("It's week day")
+}
+else if(input=="Tuesday"){
+  console.log("It's week day")
+}
+else if(input=="wednesday"){
+  console.log("It's week day")
+}
+else if(input=="Thursday"){
+  console.log("It's week day")
+}
+else if(input=="friday"){
+  console.log("It's week day")
+}
+else if(input=="saturday"){
+  console.log("It's weekend")
+}
+else if(input=="sunday"){
+  console.log("It's holiday")
+}
+else {
+  console.log("Try again")
+}*/
+ 
+//Question 19
+
+
+/*var score = +prompt("Enter score")
+if (score>=50){
+  console.log("you are passed")
+}
+else{
+  console.log("Try again")
+}*/
+
+//Question 20
+
+
+/*var number1 = +prompt("Enter number one")
+var number2 = +prompt("Enter number two")
+
+if(number1>number2){
+  console.log(number1,"is greater")
+}
+else if(number2>number1){
+  console.log(number2,"is greater")
+}
+else if(number1==number2){
+  console.log("Both equal")
+}*/
+
+//Question 21
+
+
+/*var language= prompt('english,french,urdu')
+
+if(language==="english"){
+  console.log("Hello World")
+}
+else if (language==="french"){
+  console.log("Bonjour le monde")
+}
+else if (language==="urdu"){
+  console.log("salam duniya")
+}
+else{
+  console.log("try again")
+}*/
+
+//Question 22
+
+
+/*var num = +prompt("enter number")
+
+if (num==="-"+num){
+console.log(num,"is negative")
+} 
+else  if (num===+num){
+console.log(num,"is positive")
+}*/
+
+//Question 23
+
+
+/*var item = prompt ("Enter item")
+var num =+prompt(" Number of item")
+var New = (num+' '+ item)
+console.log(New)*/
