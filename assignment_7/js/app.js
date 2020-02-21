a4f35c199e34518769f87a9f9e3d4a69bf3831aa
faizan517 <faizanpervaiz517@gmail.com>
2020-02-21 22:08:51 +0500

    assignment

---
+++ assignment_7/js/app.js
@@ -0,0 +1,46 @@
+//Task 1
+
+//var text =
+  //"the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog";
+
+/*var quikLength =4;
+var brownLength =5;
+var foxLength =3;
+var lazyLength =4;
+var dogLength =3;
+
+
+for (let i = 0; i < text.length; i++) {
+
+  if(text.slice(i,i + quikLength)=="quik"&&(i,i + brownLength)=="brown"&&(i,i + foxLength)=="fox"&&(i,i + lazyLength)=="lazy"&&(i,i + dogLength)=="dog")
+ {
+  text = text.slice(0 , i) + "black" + text.slice(i+brownLength);
+ }
+
+}
+console.log(text)*/
+
+//Task 4
+
+//var newtext = text.replace("the","A")
+//console.log(newtext)
+
+//Task 5
+
+//var newtext = text.replace(/jumps/g ,"jumping")
+//console.log(newtext)
+
+
+//Task 2
+/*for (var i = 0; i < text.length; i++) {
+  if (text.slice(i, i + 5) === "jumps")
+    text = text.slice(0, text.indexOf("jumps")) +"bounce" +text.slice(text.indexOf("jumps") + 5);
+}
+console.log(text);*/
+
+//Task 3
+/*for (var i = 0; i < text.length; i++) {
+  if (text.slice(i, i + 5) === "brown")
+    text = text.lastIndexOf("brown",0) +"black" +text.slice(text.indexOf("brown") + 5);
+} 
+console.log(text);*/
