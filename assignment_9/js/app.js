f85840d88304d875eb9563269b0f191611e8eb61
faizan517 <faizanpervaiz517@gmail.com>
2020-02-25 21:19:29 +0500

    assignment-9

---
+++ assignment_9/js/app.js
@@ -0,0 +1,36 @@
+// var num = 4.9
+// var num1 = "0";
+// var name = "faizan";
+
+//1
+//console.log(Math.round(num))//math.round use for round the number if number greater then .5 
+
+//2
+//console.log(Math.ceil(num))//math.ceil always round the number greater than
+
+//3
+//console.log(Math.floor(num))//math.floor always round the number less than
+
+//4
+//console.log(Math.random())//math.random  return random numbers 
+
+//5
+//console.log(num.toString())//to string use for convert number into string
+
+//6
+//console.log(Number(num1))//Number use for convert string into number
+
+//7
+//console.log(num.toFixed())//toFixed use for controling the length of decimals
+
+//8
+//console.log(name.toUpperCase())//toUpperCase use for capital letters
+
+//9
+//console.log(name.toLowerCase())//toLowerCase use for small letters
+
+//10
+//console.log(parseInt(num))
+
+//11
+//console.log(parseFloat(num))
