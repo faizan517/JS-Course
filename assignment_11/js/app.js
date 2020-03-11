400ad05f83902e467438d63e6f177db640b87810
faizan517 <faizanpervaiz517@gmail.com>
2020-03-11 19:50:05 +0500

    assignment-11

---
+++ assignment_11/js/app.js
@@ -0,0 +1,29 @@
+// var name = new Date()
+// console.log("TCL: name", name)
+
+// var rightNow = new Date();
+// var theDay = rightNow.getDay();
+// console.log("TCL: theDay", theDay)
+
+
+//  var Month = +prompt("Enter the number of month");
+//  var monthsList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//  console.log(monthsList[Month-1]);
+
+// var month = new Date()
+// var the = month.getMonth()+1
+// console.log(the)
+
+//  var rightNow = new Date();
+// if(rightNow<12){
+//     console.log("AM")
+// }
+// else{
+//     console.log("PM")
+// }
+
+// var userAge = +prompt("Enter your age");
+// var now = new Date().getFullYear();
+// now = Number(now);
+// var arr = now - userAge;
+// console.log(arr);
