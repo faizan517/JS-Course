26e15e28f0e88a73e87ad6c1b54d3455930f3f76
faizan517 <faizanpervaiz517@gmail.com>
2020-03-11 23:22:38 +0500

    Assignment-11

---
+++ assignments_11/js/app.js
@@ -0,0 +1,71 @@
+// var date = new Date();
+// console.log(date)
+
+// var date = new Date();
+// var day = date.getDate()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getMonth()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getDate()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getFullYear()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getHours()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getMinutes()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getSeconds()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getUTCMilliseconds()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.getTime()
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setFullYear(2016)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setHours(2)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setMonth(7)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setSeconds(2)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setUTCMilliseconds(2)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setUTCMinutes(4)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setHours(4)
+// console.log(day)
+
+// var date = new Date();
+// var day = date.setDate(4)
+// console.log(day)
+
