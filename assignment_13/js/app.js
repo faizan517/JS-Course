6e95b95341f02784eb35a48809bc52b5800b4f01
faizan517 <faizanpervaiz517@gmail.com>
2020-03-12 18:32:34 +0500

    assignment-13

---
+++ assignment_13/js/app.js
@@ -0,0 +1,65 @@
+//prameterized function
+//Local variable
+
+/*function calculator(num1, ope ,num2) {
+  switch (ope) {
+    case "+":
+      console.log(num1 + num2);
+      break;
+    case "-":
+      console.log(num1 - num2);
+      break;
+    case "*":
+      console.log(num1 * num2);
+      break;
+    case "/":
+      console.log(num1 / num2);
+      break;
+    default:
+        console.log("invalid operator")
+      break;
+  }
+}*/
+
+//function returning
+
+//calculator()
+
+
+// simple function
+
+/*function name(){
+  alert("enter your name")
+}*/
+
+
+//Access Local Variables Globally using return
+
+// function checkScope() {
+//   var lastName = "xyz"
+//   return lastName
+// }
+// console.log(lastName)
+
+
+//While loop
+
+/*var i = 1
+while (i<=10) {
+console.log(i)  
+i++  
+}*/
+
+
+//Do while loop
+
+/*var i = 1
+do{
+  console.log(i)
+  i++
+}while (i <=5) */
+
+
+//Global variable
+
+//var name = "name"
