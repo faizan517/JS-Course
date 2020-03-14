f9843c0de4b0fb7c3b15eb3de7ee86fbde56e209
faizan517 <faizanpervaiz517@gmail.com>
2020-03-14 22:12:39 +0500

    assignment-17

---
+++ assignment_12/js/app.js
@@ -0,0 +1,9 @@
+function replaceNmae(sana){
+  var names = [["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"], ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"], ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"], ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"]]
+if (names ="sana") {
+  "sana".replace
+console.log(names)  
+}
+}
+
+replaceNmae()
---
+++ assignment_17/js/app.js
@@ -0,0 +1,21 @@
+// var data = document.getElementById("txtInput")
+// console.log(data)
+
+
+
+//   var data = document.getElementsByClassName('m')
+//   for (let i = 0; i < data.length; i++) {
+//       if(i%2==0){
+//           data[i].style.color="red"
+//       }
+//       else
+//       {
+//           data[i].style.color="green"
+//           data[i].innerHTML='zindabad'
+//       }
+//   }
+
+// var data = document.getElementsByClassName('m')
+//    for (let i = 0; i < data.length; i++) {
+//        data[i].style.color="red"
+//    }
