95fee0ce941b5ed877774fb82beafd5eb62532c8
faizan517 <faizanpervaiz517@gmail.com>
2020-09-24 22:20:16 +0500

    Assignment-12

--- assignment_12/js/app.js
+++ assignment_12/js/app.js
@@ -1,9 +1,32 @@
-function replaceNmae(sana){
-  var names = [["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"], ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"], ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"], ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"]]
-if (names ="sana") {
-  "sana".replace
-console.log(names)  
-}
-}
+var names = [
 
-replaceNmae()
+    ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"],
+    ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"],
+    ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"],
+    ["Ali", "Ahmed", "Shan", "Zia", "Sana", "Khan"]
+
+];
+
+function replace(replaceWith, newValue) {
+    var arr = []
+    var nestedArr = []
+
+    for (let i = 0; i < names.length; i++) {
+        for (let j = 0; j < names[i].length; j++) {
+            if (names[i][j] === replaceWith) {
+                nestedArr.push(newValue)
+            }
+            else {
+                nestedArr.push(names[i][j])
+            }
+            arr.push(nestedArr)
+            nestedArr=[]
+
+        }
+    return arr    
+    }
+
+}
+var result = replace("Sana","Zeeshan")
+console.log(result)
+
