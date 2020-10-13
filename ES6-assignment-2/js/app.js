a9b0389e306dd66f84eca6a19ae51ccf22fdbd6f
faizan517 <faizanpervaiz517@gmail.com>
2020-10-13 17:17:11 +0500

    ESassignment-2

---
+++ ES6-assignment-2/js/app.js
@@ -0,0 +1,104 @@
+//Fetch//
+
+
+// GET//
+
+// fetch('https://jsonplaceholder.typicode.com/todos/2').then(function(response){
+//   return response.json()
+// }).then(function(result){
+//   console.log(result)
+// })
+
+
+//POST//
+
+// const payload = {
+//   firstName: 'Sultan',
+//   country: 'Pakistan',
+//   city: 'Lahore',
+//   profileIMG: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+//   phoneNumber: '1223213212',
+//   lastName: 'Naiz',
+//   email: 'Sultanaiz@gmail.com',
+//   state: 'Sindh',
+//   password: '11111'
+// }
+
+// const formdata = new FormData()
+
+// for(let key in payload){
+//   formdata.append(key,payload[key]  )
+// }
+
+// const config = {
+// method : 'POST',
+// redirect : 'follow',
+// body : formdata
+// }
+
+// fetch('https://localpages.herokuapp.com/api/auth/register',config).then(function(response){
+//   return response.json()
+// }).then(function(result){
+//   console.log(result)
+// })
+
+
+//QUERY SELECTOR//
+
+//document.querySelector("div").innerHTML = "Hello"
+
+
+// SPREAD OPERATORS//
+
+//var firstName = ["Jokhay","Talha","Younus","Savage"]
+
+//var secondNames = ["Emiway","Fadi","Demon","Divine"]
+
+//var result = [...firstName,...secondNames]
+//var result = [...firstName,...secondNames,"Bohemia","Bhari"]
+//console.log(result)
+
+//SET//
+
+//var arr = new Set(["a","b","c","d"])
+//arr.add("s")
+//arr.clear()
+//arr.delete("a")
+//console.log(arr)
+
+
+//DEFAULT PARAMETERS//
+
+// function name( n ='a', i = 'b') {
+//     console.log(n,i)
+//     return n,i
+// }
+// name()
+
+
+//REVERSE//
+
+// var nam = [1,2,3,4,5,'a','d']
+// var result =nam.reverse()
+// console.log(result)
+
+
+//ARROW FUNCTION//
+//Arrow fucntion like normal function but it,s sentex is less
+
+// var fun = (a,b) => {
+//     console.log(a+b)
+// }
+// fun(10,10)
+
+//MAP OBJECT//
+
+
+// var maiMap = new Map();
+// maiMap.set('name', 'fazi')
+// var result = maiMap.get('name')
+// console.log("result", result)
+
+ 
+
+
